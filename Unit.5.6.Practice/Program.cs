b2d7699602d9f3aaa1b2c50bc98c14b43451ac76
Mellomanprost <anoppa@mail.ru>
2022-09-10 18:13:39 +0300

    Complete practice for Module 5

--- Unit.5.6.Practice/Program.cs
+++ Unit.5.6.Practice/Program.cs
@@ -10,91 +10,194 @@ namespace Unit._5._6.Practice
     {
         static void Main(string[] args)
         {
-            GetUserInfo();
+            (string Name, string Lastname, int Age, bool HasPets, string[] PetsNames, string[] FavColors) UserData;
+            GetUserInfo(out UserData);
+            ShowUserData(UserData);
             
         }
 
-        static (string Name, string Lastname, int Age) GetUserInfo()
+        static (string Name, string Lastname, int Age, bool HasPets, string[] PetsNames, string[] FavColors) GetUserInfo(out (string Name, string Lastname, int Age, bool HasPets, string[] PetsNames, string[] FavColors) UserInfo)
         {
-            (string Name, string Lastname, int Age) User;
+            (string Name, string Lastname, int Age, bool HasPets, string[] PetsNames, string[] FavColors) User;
 
-            Console.WriteLine("Введите имя пользователя: ");
-            User.Name = Console.ReadLine();
+            string inputname;
+            string username;
+            do
+            {
+                Console.WriteLine("Введите имя пользователя: ");
+                inputname = Console.ReadLine();
 
-            Console.WriteLine("Введите фамилию пользователя: ");
-            User.Lastname = Console.ReadLine();
+            } while (CheckInpValue(inputname, out username));
+            User.Name = username;
 
-            Console.WriteLine("Введите возраст пользователя: ");
-            User.Age = int.Parse(Console.ReadLine());
+            string inputlastname;
+            string lastname;
+            do
+            {
+                Console.WriteLine("Введите фамилию пользователя: ");
+                inputlastname = Console.ReadLine();
+            } while (CheckInpValue(inputlastname, out lastname));
+            User.Lastname = lastname;
 
-            Console.WriteLine("Есть ли у вас животные? Введите Да или Нет.");
+            string age;
+            int intage;
+            do
+            {
+                Console.WriteLine("Введите возраст пользователя: ");
+                age = Console.ReadLine();
+
+            } while (CheckNum(age, out intage));
+            User.Age = intage;
+
+            Console.WriteLine("Есть ли у вас животные? \n(Введите Да или Нет):");
             string checkPets = Console.ReadLine();
             if (checkPets == "Да")
-                User.HasPet = true;
+                User.HasPets = true;
             else
-                User.HasPet = false;
+                User.HasPets = false;
 
-            if (User.HasPet == true)
+            if (User.HasPets == true)
             {
-                Console.WriteLine("Введите количество животных, которые у вас есть: ");
-                int numberOfPets = int.Parse(Console.ReadLine());
-                GetPetsName(numberOfPets);
+                string[] arraypets;
+                int numberofpets;
+                do
+                {
+                    Console.WriteLine("Введите количество животных, которые у вас есть: ");
+
+                } while (CheckNum(Console.ReadLine(), out numberofpets));
+                GetArrayWithPets(numberofpets, out arraypets);
+                User.PetsNames = arraypets;
+            }
+            else 
+            {
+                User.PetsNames = null;
             }
 
-            Console.WriteLine("Сколько у вас есть любимых цветов?");
-            int numberOfFavorColor = int.Parse(Console.ReadLine());
-            GetFavorColor(numberOfFavorColor);
+            string[] arrayfavcolors;
+            int numoffavcolor;
+            do
+            {
+                Console.WriteLine("Сколько у вас есть любимых цветов?");
 
+            } while (CheckNum(Console.ReadLine(), out numoffavcolor));
+            WriteArrayOfFavColors(numoffavcolor, out arrayfavcolors);
+            User.FavColors = arrayfavcolors;
+            UserInfo = User;
+
+            return UserInfo;
         }
 
-        static void CheckNUm(string age, out int num)
+        static string[] GetArrayWithPets(int num, out string[] arrayOfPetsNames)
         {
-            TryParse;
-            if (age > 0)
+            var petsNames = new string[num];
+            string inputPetName;
+            for (int i = 0; i < petsNames.Length; i++)
             {
+                do
+                {
+                    Console.WriteLine("Запишите кличку животного: ");
+                    inputPetName = Console.ReadLine();
 
+                } while (CheckInpValue(inputPetName, out petsNames[i]));
+                
             }
+
+            arrayOfPetsNames = petsNames;
+            return arrayOfPetsNames;
         }
 
-        static string[] GetPetsName(int num)
+        static string[] WriteArrayOfFavColors(int num, out string[] arrayOfFavorColors)
         {
-            var arrayOfPetsName = new string[num];
-            for (int i = 0; i < arrayOfPetsName.Length; i++)
+            var arrayColors = new string[num];
+            string inputdata;
+            for (int i = 0; i < arrayColors.Length; i++)
             {
-                Console.WriteLine("Запишите кличку животного номер {0}: ", i);
-                arrayOfPetsName[i] = Console.ReadLine();
+                do
+                {
+                    Console.WriteLine("Введите ваш любимый цвет:");
+                    inputdata = Console.ReadLine();
+                } while (CheckInpValue(inputdata, out arrayColors[i]));
             }
 
-            Console.WriteLine("Ваших питомцев зовут: ");
-            for (int j = 0; j < arrayOfPetsName.Length; j++)
-            {
-                Console.Write("{0} ", arrayOfPetsName[j]);
-            }
-            Console.WriteLine();
-            return arrayOfPetsName;
+            arrayOfFavorColors = arrayColors;
+            return arrayOfFavorColors;
         }
 
-        static string[] GetFavorColor(int num)
+        static bool CheckNum(string number, out int corrnumber)
         {
-            var arrayOfFavorColor = new string[num];
-            for (int i = 0; i < arrayOfFavorColor.Length; i++)
+            if (int.TryParse(number, out int intnum))
             {
-                Console.WriteLine("Введите ваш любимый цвет номер {0}", i);
-                arrayOfFavorColor[i] = Console.ReadLine();
+                if(intnum > 0)
+                {
+                    corrnumber = intnum;
+                    return false;
+                }
             }
-
-            Console.WriteLine("Ваши любимые цвета: ");
-            for (int j = 0; j < arrayOfFavorColor.Length; j++)
             {
-                Console.Write("{0} ", arrayOfFavorColor[j]);
+                corrnumber = 0;   // не понимаю зачем здесь нужны скобки {}
+                Console.WriteLine("Введенные данные не корректны!");
+                return true;
             }
-            Console.WriteLine();
-            return arrayOfFavorColor;
         }
 
-        static void CheckEnteredData()
+        static bool CheckInpValue(string input, out string corrinput)
         {
-            
+            //if (input == null)
+            //{
+            //    Console.WriteLine("Данные отсутствуют!");
+            //    corrinput = null;
+            //    return true;
+            //}
+            if (int.TryParse(input, out int intvalue))
+            {
+                Console.WriteLine("Введены неверные данные!");
+                corrinput = null;
+                return true;
+            }
+            else 
+            {
+                corrinput = input;
+                return false;
+            }
+                
         }
+
+        static void ShowUserData((string Name, string Lastname, int Age, bool HasPets, string[] PetsNames, string[] FavColors) User)
+        {
+            Console.WriteLine("Имя: " + User.Name);
+            Console.WriteLine("Фамилия: " + User.Lastname);
+            Console.WriteLine("Возраст: " + User.Age);
+
+            Console.WriteLine("Есть ли животные: " + (User.HasPets ? "Да" : "Нет"));
+            if (User.HasPets)
+            {
+                Console.WriteLine("Клички животных: ");
+                for (int i = 0; i < User.PetsNames.Length; i++)
+                {
+                    Console.Write(User.PetsNames[i]);
+                    if (i != User.PetsNames.Length - 1)
+                    {
+                        Console.Write(", ");
+                    }
+                }
+                Console.WriteLine(".");
+            }
+            else
+                Console.WriteLine("Животных нет.");
+
+            Console.WriteLine("Любимые цвета: ");
+            for (int j = 0; j < User.FavColors.Length; j++)
+            {
+                Console.WriteLine(User.FavColors[j]);
+                if (j != User.PetsNames.Length - 1)
+                {
+                    Console.Write(", ");
+                }
+            }
+            Console.WriteLine(".");
+
+        }
+
     }
+    
 }
